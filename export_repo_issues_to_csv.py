6f4fc2729f4a36c4e663da5649d911fccf0d3d00
Tobias Nteireho <tobias@cali.org>
2019-10-14 14:58:06 -0500

    remove credentials from code

--- export_repo_issues_to_csv.py
+++ export_repo_issues_to_csv.py
@@ -8,37 +8,45 @@ import csv
 import requests
 
 
-GITHUB_USER = ''
-GITHUB_PASSWORD = ''
-REPO = ''  # format is username/repo
-ISSUES_FOR_REPO_URL = 'https://api.github.com/repos/%s/issues' % REPO
+ISSUES_FOR_REPO_URL = 'https://api.github.com/repos/%s/issues?state=all' % REPO
 AUTH = (GITHUB_USER, GITHUB_PASSWORD)
 
 def write_issues(response):
     "output a list of issues to csv"
+    #print(r.json())
     if not r.status_code == 200:
         raise Exception(r.status_code)
     for issue in r.json():
-        labels = issue['labels']
-        for label in labels:
-            if label['name'] == "Client Requested":
-                csvout.writerow([issue['number'], issue['title'].encode('utf-8'), issue['body'].encode('utf-8'), issue['created_at'], issue['updated_at']])
+        #labels = issue['labels']
+        #for label in labels:
+        #if label['name'] == "Client Requested":
+        csvout.writerow([issue['number'], issue['url'], issue['state'], issue['title'], issue['body'], issue['created_at'], issue['updated_at'], issue['closed_at']])
 
 
 r = requests.get(ISSUES_FOR_REPO_URL, auth=AUTH)
+#print(r.content)
 csvfile = '%s-issues.csv' % (REPO.replace('/', '-'))
-csvout = csv.writer(open(csvfile, 'wb'))
-csvout.writerow(('id', 'Title', 'Body', 'Created At', 'Updated At'))
+f = open(csvfile, 'w', encoding='utf-8')
+csvout = csv.writer(f)
+csvout.writerow(('id', 'url', 'state', 'Title', 'Body', 'Created At', 'Updated At', 'Closed At'))
 write_issues(r)
-
+page = 0
 #more pages? examine the 'link' header returned
-if 'link' in r.headers:
-    pages = dict(
-        [(rel[6:-1], url[url.index('<')+1:-1]) for url, rel in
-            [link.split(';') for link in
-                r.headers['link'].split(',')]])
-    while 'last' in pages and 'next' in pages:
+while (1):
+    if 'link' in r.headers:
+        pages = dict(
+            [(rel[6:-1], url[url.index('<')+1:-1]) for url, rel in
+                [link.split(';') for link in
+                    r.headers['link'].split(',')]])
+                    
+        #while 'last' in pages and 'next' in pages:
+            #print(r.headers['link'])
+        print('page %i\nlink'%page,pages['next'])
+        page = page +1
         r = requests.get(pages['next'], auth=AUTH)
+        #print(r.headers)
         write_issues(r)
         if pages['next'] == pages['last']:
             break
+            
+f.close()
